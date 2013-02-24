f87e0048b45f82732fb28a61337c2216d9f5303f
aakpat6 <aakpat6@gmail.com>
2013-02-23 19:40:33 -0500

    Updated comments and style. Made an Obstacle() constructor to make instantiating obstacles easier.

--- squarulous.js
+++ squarulous.js
@@ -1,3 +1,7 @@
+/* Initialize HTML elements */
+/* TODO: Deal with canvas resize */
+/* TODO: Implement collision detection between obstacles */
+
 var canvas = document.getElementById("myCanvas");
 canvas.setAttribute('tabindex', '0');
 canvas.width = window.innerWidth;
@@ -5,7 +9,7 @@ canvas.height = window.innerHeight;
 canvas.focus();
 var ctx = canvas.getContext("2d");
 
-
+/* Make the 3 objects */
 var hero = {
 	speed: 5,
 	x: canvas.width/2,
@@ -17,10 +21,27 @@ var target = {
 	y: 0
 };
 
+function Obstacle() {	
+	var speedfactor = 5;
+	var tmpdy = Math.random() * speedfactor;
+	var tmpdx = Math.random() * speedfactor;
+	this.x = 32 + (Math.random() * (canvas.width - 64));
+	this.y = 32 + (Math.random() * (canvas.height - 64));
+	this.dx = tmpdx;
+	this.dy = tmpdy;
+	
+	/* Start the obstacle on the top or the right? */
+	var randStart = Math.floor(Math.random() * 2);
+	if (randStart == 0) this.y = 5;
+	else this.x = 5;
+}
+
+/* Initialize variables */
 var obstacles = [];
 var score = 0;
 var keysDown = {};
 
+/* Key listeners */
 canvas.addEventListener("keydown", function (e) {
 	keysDown[e.keyCode] = true;
 }, false)
@@ -29,21 +50,26 @@ canvas.addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false)
 
+/* Moves the target to a random location on the canvas */
 function moveTarget() {
 	target.x = 64 + (Math.random() * (canvas.width - 96));
 	target.y = 64 + (Math.random() * (canvas.height - 96));
 }
 
 function moveHero() {
+	/* Up */
 	if (38 in keysDown && hero.y > 0) {
 		hero.y -= hero.speed;
 	}
+	/* Down */
 	if (40 in keysDown && hero.y < canvas.height-32) {
 		hero.y += hero.speed;
 	}
+	/* Left */
 	if (37 in keysDown && hero.x > 0) {
 		hero.x -= hero.speed;
 	}
+	/* Right */
 	if (39 in keysDown && hero.x < canvas.width-32) {
 		hero.x += hero.speed;
 	}
@@ -56,26 +82,15 @@ function moveHero() {
 	{
 		score++;
 		moveTarget();
-
-		var upDown = Math.floor(Math.random() * 2);
-		var tmpdy = Math.random() * 5;
-		var tmpdx = Math.random() * 5;
-		
-		var newObstacle = {
-			x: 32 + (Math.random() * (canvas.width - 64)),
-			y: 32 + (Math.random() * (canvas.height - 64)),
-			dx: tmpdx,
-			dy: tmpdy,
-		};
-		if (upDown == 0) newObstacle.y = 5;
-		else newObstacle.x = 5;
-		obstacles.push(newObstacle);
+		obstacles.push(new Obstacle());
 	}
 }
 
+/* Moves each of the obstacles one step in their respective directions */
 function moveObstacles() {
+	/* First, move all the obstacles */
 	for (var i = 0; i < obstacles.length; i++) {
-		o = obstacles[i];
+		var o = obstacles[i];
 		if (!(o.x > 0 && o.x < canvas.width-30)) {
 			o.dx *= -1;
 		}
@@ -84,71 +99,72 @@ function moveObstacles() {
 		}
 		o.x += o.dx;
 		o.y += o.dy;
-
+		
+		/* If an obstacle intersects the hero, it's game over */
 		if (hero.x <= o.x + 30
 		&& o.x <= (hero.x + 30)
 		&& hero.y <= (o.y + 30)
 		&& o.y <= (hero.y + 30))
 		{
-			keysDown = {};
-			
-			/*ctx.fillStyle = "#000000";
-			ctx.fillRect(0,0,canvas.width,canvas.height);
-
-			ctx.fillStyle = "#ffffff";
-			ctx.font = "60px Arial";
-			ctx.textAlign = "center";
-			ctx.fillText("Game Over", canvas.width/2, 100);
-			ctx.fillText("Score: " + score, canvas.width/2, 200);
-			ctx.drawText*/
-
+			keysDown = {}; // Stop moving
 			alert("Game Over\nScore: " + score);
 
+			/* Reinit vars */
 			obstacles = [];
 			score = 0;
-			movetarget();
+			moveTarget();
 			hero.x = canvas.width/2;
 			hero.y = canvas.height/2;
 		}
 	}
 }
 
+/* What happens every step of the game */
 function update() {
 	moveHero();
 	moveObstacles();
 }
 
+/* Actually displays where everything is */
 function render() {
+	/* Background is black */
 	ctx.fillStyle = "#000000";
 	ctx.fillRect(0,0,canvas.width,canvas.height);
 
+	/* Hero is green */
 	ctx.fillStyle = "#00ff00";
 	ctx.fillRect(hero.x, hero.y, 32,32);
 
+	/* Target is blue */
 	ctx.fillStyle = "#0000ff";
 	ctx.fillRect(target.x, target.y, 30, 30);
 
+	/* Obstacles are red */
 	ctx.fillStyle = "#ff0000";
 	for (var i = 0; i < obstacles.length; i++) {
-		o = obstacles[i];
+		var o = obstacles[i];
 		ctx.fillRect(o.x,o.y,30,30);
 	}
 
+	/* Display the score at the top */
 	ctx.fillStyle = "#ffffff";
 	ctx.font = "30px Arial";
 	ctx.textAlign = "center";
 	ctx.fillText("Score: " + score, canvas.width/2,30);
 }
 
-
+/* Main game loop; calls all other functions */
 function main() {
-	setTimeout(main, 10);
+	setTimeout(main, 10); // Makes sure main will happen every 10 ms
+	
 	update();
 	render();
+	
+	/* Makes sure that the update is occurring as it should */
 	console.log(new Date().getMilliseconds() - last);
 	last = new Date().getMilliseconds();
 }
 
-moveTarget();
+moveTarget(); // Initialize the target
 var last = new Date().getMilliseconds();
-main();
+main(); // Start the main game loop
